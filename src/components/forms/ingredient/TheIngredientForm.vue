c579eb606d650e3e8f4fcaf74fd5112e192ecbd6
Rachel <rlmilk@aol.com>
2019-04-15 11:07:28 -0400

    finish ingredient form

--- src/components/forms/ingredient/TheIngredientForm.vue
+++ src/components/forms/ingredient/TheIngredientForm.vue
@@ -1,41 +1,46 @@
 <template>
-   <the-form-template :type="'ingredient'"> 
-      <div id='topbar' slot='topbar-content'>
-         <span class='warning' v-if='this.warning !== ""'>{{ warning }}</span>
-         <input class='btn' @click.prevent='saveIngredient()' type='submit' value='Save'>
-         <a v-if='id !== undefined' class='btn btn-delete' @click='deleteIngredient()'>Delete Ingredient</a>
-         <router-link :to="{ name: 'List', params: { type: 'Ingredient' } }" 
-                      class='btn'><a>Cancel</a>
-         </router-link>   
-      </div>
-
-      <div slot='form-body' >
-         <div id='ingredient-details'>
-            <h2>{{ id === "" ? 'New' : 'Edit' }} Ingredient</h2>
-            <label class='min-width' for='description'>Description *</label>
-            <input autofocus type='text' name='description' v-model='ingredient.Description'><br>
-
-            <label class='min-width' for='qty'>Quantity *</label>
-            <input type='number' name='qty' v-model='ingredient.Quantity'>
-
-            <label for='unit'>Unit </label>
-            <select name='unit' v-model='ingredient.Unit.Id'>
-               <option v-for='u in units' 
-                       :key='u.Id' 
-                       :value='u.Id'>{{ u.Description }}
-               </option>
-            </select>
-
-            <label for='cost'>Cost $ *</label>
-            <input type='number' step='0.01' name='cost' v-model='ingredient.Cost'>
+   <div>
+      <app-modal :showModal='showModal' :text='modalText'></app-modal>
+      <the-form-template :type="'ingredient'"> 
+         <div id='topbar' slot='topbar-content'>
+            <span class='warning' v-if='this.warning !== ""'>{{ warning }}</span>
+            <input class='btn' @click.prevent='submit()' type='submit' value='Save'>
+            <a v-if='id !== undefined' class='btn btn-delete' @click='deleteIngredient()'>Delete Ingredient</a>
+            <router-link :to="{ name: 'List', params: { type: 'Ingredient' } }" 
+                        class='btn'><a>Cancel</a>
+            </router-link>   
          </div>
-      </div>
-   </the-form-template>
+
+         <div slot='form-body' >
+            <div id='ingredient-details'>
+               <h2>{{ id === "" ? 'New' : 'Edit' }} Ingredient</h2>
+               <label class='min-width' for='description'>Description *</label>
+               <input autofocus type='text' name='description' v-model='ingredient.Description'><br>
+
+               <label class='min-width' for='qty'>Quantity *</label>
+               <input type='number' name='qty' v-model='ingredient.Quantity'>
+
+               <label for='unit'>Unit </label>
+               <select name='unit' v-model='ingredient.Unit.Id'>
+                  <option v-for='u in units' 
+                        :key='u.Id' 
+                        :value='u.Id'>{{ u.Description }}
+                  </option>
+               </select>
+
+               <label for='cost'>Cost $ *</label>
+               <input type='number' step='0.01' name='cost' v-model='ingredient.Cost'>
+            </div>
+         </div>
+      </the-form-template>
+   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { eventBus } from '../../../main.js';
 import TheFormTemplate from '../TheFormTemplate';
+import AppModal from '../../shared/AppModal';
 
 export default {
    props: {
@@ -46,6 +51,11 @@ export default {
          // display data
          warning: '',
 
+         /* Modal fields */
+         modalText: '',
+         showModal: false,
+         callback: Object,
+         
          // ingredient data
          ingredient: {
             Description: '',
@@ -67,24 +77,42 @@ export default {
                this.ingredient = data;
             }, error => console.log(error));
       },
-      saveIngredient() {
-         if(this.description === '' || this.quantity <= 0 || this.cost <= 0) {
+      submit() {
+         if(this.ingredient.Description === '' || this.ingredient.Quantity <= 0 || this.ingredient.Cost <= 0) {
             this.warning = 'Please include all required fields.';
-         }
-         else {
-            alert(`${this.description}, ${this.quantity}, ${this.unit}, ${this.cost}`);
-            this.resetForm();
+         } else {
+            if(this.id) {
+               this.$http.put(`ingredient/${this.id}`, this.ingredient)
+                  .then(response => { 
+                     console.log(`successfully saved ingredient`)
+                  }, error => console.log(error))
+                  .then(() => {
+                     this.$router.push({name: 'List', params: { type: 'Ingredient' }});
+                  });
+            } else {
+            this.$http.post(`ingredient`, this.ingredient)
+               .then(response => { 
+                  console.log(`successfully added ingredient`)
+               }, error => console.log(error))
+               .then(() => {
+                  this.$router.push({name: 'List', params: { type: 'Ingredient' }});
+               });
+            }
          }
       },
       deleteIngredient() {
-         alert('delete this ingredient: ', this.description);
-         this.resetForm();
-      },
-      resetForm() {
-         this.description = '';
-         this.quantity = 0;
-         this.unit = 1;
-         this.cost = 0;
+         this.modalText = 'Are you sure you want to delete this ingredient?';
+         this.showModal = true;
+         this.callback = 
+            function() { 
+               this.$http.delete(`ingredient/${this.id}`)
+                  .then(response => { 
+                     console.log(`successfully deleted ingredient`)
+                  }, error => console.log(error))
+                  .then(() => {
+                     this.$router.push({name: 'List', params: { type: 'Ingredient' }});
+                  });
+            };
       }
    },
    computed: {
@@ -96,9 +124,17 @@ export default {
       if(this.id) {
          this.getIngredient();
       }
+
+      eventBus.$on('modalResponse', (response) => {
+         this.showModal = false;
+         if(response) {
+            this.callback();               
+         }
+      });
    },
    components: {
-      'the-form-template': TheFormTemplate
+      'the-form-template': TheFormTemplate,
+      'app-modal': AppModal
    }
 }
 </script>
